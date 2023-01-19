3b01083ffe727afa203ed02697b95c23f963b1df
RejoyanIslam <rejoyanislam0014@gmail.com>
2023-01-20 01:24:03 +0600

    upload

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,11 @@ const loaderPercent = document.querySelector(".loaderPercent");
 const loader = document.getElementById("loader");
 let percent = 0;
 const clear = setInterval(() => {
- 
-  loader.style.width=`${percent}%`
+  loader.style.width = `${percent}%`;
   if (percent == 100) {
-    loader.innerHTML='Completed!'
+    loader.innerHTML = "Completed!";
     clearInterval(clear);
   }
   loaderPercent.innerHTML = `${percent}%`;
-   percent++;
+  percent++;
 }, 40);
